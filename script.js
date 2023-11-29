a96ef2d743dabf274f5d9254d7f0b4aca2e35286
Aryan-Agrawal <142228203+Aryanchamp@users.noreply.github.com>
2023-11-29 22:53:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,43 @@
-async function fetchAirQualityData() {
-  const url = 'https://air-quality-by-api-ninjas.p.rapidapi.com/v1/airquality?city=';
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': 'afa0abaa54msha14d99c84c3628dp1abc27jsn88ee31efe96a',
-      'X-RapidAPI-Host': 'air-quality-by-api-ninjas.p.rapidapi.com'
-    }
-  };
+const url = 'https://air-quality-by-api-ninjas.p.rapidapi.com/v1/airquality?city=';
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'afa0abaa54msha14d99c84c3628dp1abc27jsn88ee31efe96a',
+    'X-RapidAPI-Host': 'air-quality-by-api-ninjas.p.rapidapi.com'
+  }
+};
+
+async function getAirQuality(city) {
+  const cityName = document.getElementById('cityName');
+  cityName.innerHTML = city;
 
   try {
-    const response = await fetch(url, options);
-    const result = await response.text();
-
+    const response = await fetch(url + city, options);
+    const result = await response.json();
 
     console.log(result);
 
-    CO.innerHTML = response.CO
-    NO2.innerHTML = response.NO2
-    O3.innerHTML = response.O3
-    SO2.innerHTML = response.SO2
-    PM10.innerHTML = response.PM10
-    overall_aqi.innerHTML = response.overall_aqi
-
-
+    document.getElementById('CO').innerHTML = result.CO.concentration;
+    document.getElementById('NO2').innerHTML = result.NO2.concentration;
+    document.getElementById('O3').innerHTML = result.O3.concentration;
+    document.getElementById('SO2').innerHTML = result.SO2.concentration;
+    document.getElementById('PM10').innerHTML = result.PM10.concentration;
+    document.getElementById('overall_aqi').innerHTML = result.overall_aqi;
   } catch (error) {
     console.error(error);
   }
 }
 
-fetchAirQualityData();
+document.addEventListener('DOMContentLoaded', () => {
+  getAirQuality('Punjab'); // Display Punjab as default city when page loads
 
-
-
+  const form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const cityInput = document.getElementById('city');
+    const city = cityInput.value.trim();
+    if (city !== '') {
+      getAirQuality(city);
+    }
+  });
+});
